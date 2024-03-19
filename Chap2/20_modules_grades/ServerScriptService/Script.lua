b26d1baee2dee138a83a74faea78e220bf144d83
SlimeVRX <99313947+SlimeVRX@users.noreply.github.com>
2024-03-19 12:08:58 +0700

    First setup Rojo

--- Chap2/20_modules_grades/ServerScriptService/Script.lua
+++
@@ -1,8 +0,0 @@
-local ServerStorage = game:GetService("ServerStorage")
-
-local StudentSystem = require(ServerStorage.StudentSystem)
-
-StudentSystem:addGrade("Tin", 5)
-StudentSystem:addGrade("Sophie", 6)
-StudentSystem:addGrade("Sophie", 8)
-StudentSystem:addGrade("William", 8)
