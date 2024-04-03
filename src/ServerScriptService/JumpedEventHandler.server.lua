22ec4836a3682112661c12341962d99214507dff
SlimeVRX <99313947+SlimeVRX@users.noreply.github.com>
2024-04-04 01:41:42 +0700

    Chap3 - Listening to events

--- src/ServerScriptService/JumpedEventHandler.server.lua
+++ src/ServerScriptService/JumpedEventHandler.server.lua
@@ -2,15 +2,8 @@
 
 local Players = game:GetService("Players")
 
-function findPlayer()
-    -- Getting all players in the game
-    local player = Players:GetPlayers()
-
-    -- Looping through all players
-    for i, player in ipairs(player) do
-        print("Found player: " .. player.Name)
-    end    
+function playerJoined(player)
+    print(player.Name .. " has joined the game!")
 end
 
-task.wait(1)
-findPlayer()
+Players.PlayerAdded:Connect(playerJoined)
