7f15fcf18355d649cfa1fcdd0258c04291a76ab7
SlimeVRX <99313947+SlimeVRX@users.noreply.github.com>
2024-04-04 02:12:28 +0700

    Chap3 - Listening to events - Nested function - Getting the Humanoid - Listening to the Jump property - Change the character color

--- src/ServerScriptService/JumpedEventHandler.server.lua
+++ src/ServerScriptService/JumpedEventHandler.server.lua
@@ -12,6 +12,9 @@ function playerJoined(player)
             humanoid:GetPropertyChangedSignal("Jump"):Connect(function()
                 if humanoid.Jump then
                     print(player.Name .. " jumped!")
+
+                    -- Change the character's color
+                    changeCharacterColor(character)
                 end
             end)
         end
@@ -19,6 +22,26 @@ function playerJoined(player)
 
     -- Listen for the .characterAdded event
     player.CharacterAdded:Connect(characterAdded)
+end
+
+function changeCharacterColor(character)
+    -- Random color
+    local newColor = getRandomColor()
+
+    -- Looping through all BaseParts in the character
+    for _, basePart in ipairs(character:GetChildren()) do
+        if basePart:IsA("BasePart") then
+            basePart.Color = newColor
+        end
+    end
+end
+
+function getRandomColor()
+    return Color3.fromRGB(
+        math.random(0, 255),
+        math.random(0, 255),
+        math.random(0, 255)
+    )
 end
 
 Players.PlayerAdded:Connect(playerJoined)
