0e73dad1b1e34c877d13b63721e4e5fa09b7f927
SlimeVRX <99313947+SlimeVRX@users.noreply.github.com>
2024-04-04 01:34:23 +0700

    Chap3 - Functions on instances - FindFirstChildOfClass instead of FindFirstChild

--- src/ServerScriptService/JumpedEventHandler.server.lua
+++ src/ServerScriptService/JumpedEventHandler.server.lua
@@ -2,14 +2,12 @@
 
 local Players = game:GetService("Players")
 
-function findPlayer(username)
-    local player = Players:FindFirstChild(username)
+function findPlayer()
+    local player = Players:FindFirstChildOfClass("Player")
     if player ~= nil then
         print("Found player: " .. player.Name)
-    else
-        print("Player not found " .. username)
     end
 end
 
 task.wait(5)
-findPlayer("SlimeVRX00")
+findPlayer()
